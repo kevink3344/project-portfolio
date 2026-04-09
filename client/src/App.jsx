5b953be8615f08b36d870abe108c97ee60d56a0d
Kevin Key <key.kevin@gmail.com>
2026-04-09 15:20:15 -0400

    Change Integrations header icon to bolt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
               title="Integrations"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9 3v2m6-2v2m6 0a2 2 0 012 2v2h1a1 1 0 011 1v3a1 1 0 01-1 1H4a1 1 0 01-1-1v-3a1 1 0 011-1h1V7a2 2 0 012-2h6a2 2 0 012 2v2a1 1 0 001 1h1a1 1 0 011 1v1a1 1 0 001 1h1v-3a2 2 0 012-2z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
               </svg>
             </a>
             <ThemeToggle />
